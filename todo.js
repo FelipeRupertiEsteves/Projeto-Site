0478920acf0bc0caecc4ec70363e7a079acb8f18
Felipe Ruperti Esteves <felipeesteves91@gmail.com>
2021-04-13 10:12:26 -0300

    to-do list done

--- todo.js
+++ todo.js
@@ -1,12 +1,34 @@
+var container3 = document.getElementsByClassName("container-3")[0];
+var i = 0;
 
-const criarItem = () => {
-    const item = document.createElement('label');
-    item.classList.add(todoItem);
-    item.innerHTML = `
-        <input type="checkbox">
-        <div>teste iten 1</div>
-        <input type="button" value="X">
-    `
+function submitToDo(){
+    var to_do = document.getElementById("to-do").value;
+    var node0 = document.createElement("div");
+    var node1 = document.createElement("input");
+    var node2 = document.createElement ("p");
 
-    document.getElementById('todoList').appendChild(item);
+    node1.type = "checkbox";
+    node2.innerHTML = to_do;
+
+    node0.appendChild(node1);
+    node0.appendChild(node2);
+
+    container3.insertAdjacentElement("beforeend",node0);
+
+    node2.style.marginLeft = "45px";
+    node0.setAttribute("style", "border-radius:5px;padding:15px 10px 5px 20px; color#434a54;margin-top:10px; font-size:25px; overflow:hidden")
+    node0.stylebackground = random_color();
+
+    node0.addEventListener("dblclick", function(){
+        node0.remove();
+    })
+}
+
+function random_color(){
+    var random_colors = ['rgb(224, 242, 233)','rgb(206, 181, 167)','rgb(161, 124, 107)','rgb(91, 123, 122)'];
+
+    if (i > random_colors.length - 1){
+        i = 0;
+    }
+    return random_color[i++]
 }
