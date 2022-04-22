b169f3b680ebb040f154c1331df19421cfeb243e
Felipe Ruperti Esteves <52058164+FelipeRupertiEsteves@users.noreply.github.com>
2022-04-22 12:07:03 -0300

    directory update
    updated directory and faveicon.

---
+++ Calculadora/calculadora.js
@@ -0,0 +1,19 @@
+// Calculadora
+var valor;
+var resultado;
+
+function botao(num) {
+    valor = document.calc.visor.value += num;
+}
+
+function reseta() {
+    document.calc.visor.value = '';
+}
+
+function calcula() {
+    resultado = eval(valor);
+    if (resultado == "Infinity") {
+        document.calc.visor.value = 'Erro. Não pode dividir por zero.';            
+    }
+    else    document.calc.visor.value = resultado;
+}
